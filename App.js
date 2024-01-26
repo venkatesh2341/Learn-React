0c021cd687b6ae73cbea8209faa9eaf8bc75cdda
Venkatesh karri <venkateshkarri@Venkateshs-MacBook-Air.local>
2024-01-26 10:18:45 +0530

    Episode 2 scripts in package.json , parcel

--- App.js
+++ App.js
@@ -2,12 +2,16 @@
 // const root= ReactDOM.createRoot(document.getElementById("root"));
 // root.render(heading);
 
+import React from "react";
+import ReactDOM from "react-dom";
+
+
 const parent= React.createElement("parent", { id: "parent"}, 
 
     [
         React.createElement("child", {id:"child"},
             [
-                React.createElement("h1", {}, "I'm a h1 tag insed two divs"),
+                React.createElement("h1", {}, "I'm a h1 tag insed two div"),
                 React.createElement("h2", {}, "I'm a h2 tag insed two divs and sibling of h1")
             ]
         ),
@@ -20,11 +24,6 @@ const parent= React.createElement("parent", { id: "parent"},
 
     ]
 )
-
-
-
-
- 
 
 const root= ReactDOM.createRoot(document.getElementById("root"));
 root.render(parent);
