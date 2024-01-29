3bb2c812b51588b61bed8616a32fe77d444920ec
Venkatesh karri <venkateshkarri@Venkateshs-MacBook-Air.local>
2024-01-29 10:54:23 +0530

    Login/Logot button is added

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,6 +1,9 @@
+import React, { useState } from "react";
 import {LOGO_LINK} from "../utils/constants";
+import { useState } from "react";
 
 const Header = () => {
+    let  [btnName, setBtnName]= useState( "Login");
     return (
         <div className= "header">
             <div className = "logo-container">
@@ -14,6 +17,12 @@ const Header = () => {
                     <li>About Us</li>
                     <li>Contact Us</li>
                     <li>Cart</li>
+                    <li>
+                        <button className="login-btn" onClick={ ()=>{
+                            setBtnName((btnName==="Login")?"Logout":"Login");
+                        }}>{btnName}</button>
+                    </li>
+                    
                 </ul>
             </div>
         </div>
