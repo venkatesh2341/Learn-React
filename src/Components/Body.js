7b71017f5be6e99bdbe801f01d8eda2d005cc585
Venkatesh karri <venkateshkarri@Venkateshs-MacBook-Air.local>
2024-01-29 10:02:01 +0530

    fectched data from external API using fetch function. Used useEffect hook for re-rendering the page

--- src/Components/Body.js
+++ src/Components/Body.js
@@ -1,15 +1,28 @@
 import RestoList from "../utils/mockData";
 import RestaurantCard from "./RestaurantCard";
-import { useState } from "react";
+import { useState , useEffect } from "react";
 
 const Body = () => {
-    let [listOfRestaurants, setListOfRestaurants] = useState(RestoList);
+    let [listOfRestaurants, setListOfRestaurants] = useState([]);
+
+    useEffect(()=>{
+        fetchData();
+    }, [])
+
+    const fetchData = async ()=>{
+        const info = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=17.4400802&lng=78.3489168&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING");
+        const json= await info.json();
+        console.log(info); 
+        setListOfRestaurants(json.data.cards[4].card.card.gridElements.infoWithStyle.restaurants);
+
+    };
+
     return (
         <div className= "body">
             <div className= "filter">
                 <button className="filter-btn"  
                     onClick={ () => {
-                        let topRatedList = listOfRestaurants.filter(restaurant => restaurant.info.avgRating >= 4)
+                        let topRatedList = listOfRestaurants.filter(restaurant => restaurant.info.avgRating > 4.2)
                         setListOfRestaurants(topRatedList);
                     } }
                 >Top Rated Restaurants</button>
@@ -23,9 +36,9 @@ const Body = () => {
                         )
                 )
             }
-            </div>
-        </div>
-    )
-}
+            </div>                   
+        </div>            
+    )                           
+}                  
 
 export default Body;
