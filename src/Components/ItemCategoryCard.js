9bbb912591b6cc7b6992481d994f232a94eae711
Venkatesh karri <venkateshkarri@Venkateshs-MacBook-Air.local>
2024-02-10 11:09:25 +0530

    Fixed the bug: accordian shouldn't open when items are zero

--- src/Components/ItemCategoryCard.js
+++ src/Components/ItemCategoryCard.js
@@ -8,23 +8,23 @@ const ItemsCategoryCard = ({itemType, showItems, SetShowItemsIndex, SetShowItems
     const [hideButton , setHideButton] = useState("🔽");
     return (
         <li className= "text-center my-4 p-2 border-2 shadow-lg "> 
-        <div className="justify-between flex cursor-pointer" onClick={()=>{
-            // if(itemType?.card?.card?.itemCards?.length > 0)
-            //     setHideItems(!hideItems);
-            showItems? SetShowItemsNull(): SetShowItemsIndex()
-            setHideButton(hideButton=== "🔽" ? "🔼" : "🔽")
-        }}>
-            <div className="font-bold"> {itemType?.card?.card?.title} ({itemType?.card?.card?.itemCards?.length || 0}) </div>
-            <div>{hideButton}</div>
-        </div>
-        { showItems=== true &&  <div className="items-list">
-            {
-                itemType?.card?.card?.itemCards.map( itemCard => ( 
-                    <ItemCard key={itemCard?.card?.info?.id} itemCard = {itemCard} />
-                ))
+            <div className="justify-between flex cursor-pointer" onClick={()=>{
+                // if(itemType?.card?.card?.itemCards?.length > 0)
+                //     setHideItems(!hideItems);
+                showItems ? SetShowItemsNull(): SetShowItemsIndex()
+                setHideButton(hideButton=== "🔽" ? "🔼" : "🔽")
+            }}>
+                <div className="font-bold"> {itemType?.card?.card?.title} ({itemType?.card?.card?.itemCards?.length || 0}) </div>
+                <div>{hideButton}</div>
+            </div>
+            { showItems=== true && itemType?.card?.card?.itemCards?.length != null && <div className="items-list">
+                {
+                    itemType?.card?.card?.itemCards.map( itemCard => ( 
+                        <ItemCard key={itemCard?.card?.info?.id} itemCard = {itemCard} />
+                    ))
+                }
+            </div>
             }
-        </div>
-        }
         </li>
     )
 }
